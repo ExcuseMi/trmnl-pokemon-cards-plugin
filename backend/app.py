52898072bd04c1f32b1270864219a9a77119806d
excuseme <excuseme@example.com>
2026-05-01 16:46:25 +0200

    First commit

--- backend/app.py
+++ backend/app.py
@@ -90,6 +90,7 @@ async def sets():
 def _cors(response: Response) -> Response:
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
     return response
 
 
